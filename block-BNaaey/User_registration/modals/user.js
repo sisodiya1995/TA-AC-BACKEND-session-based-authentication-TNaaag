ace4f0a68c45536f906f74b3d9f57c2fcbf40c8e
Akash Singh <akashingh95@gmail.com>
2022-04-19 22:27:31 +0530

    login

---
+++ block-BNaaey/User_registration/modals/user.js
@@ -0,0 +1,38 @@
+const { text } = require('express');
+var mongoose = require('mongoose')
+var schema = mongoose.Schema;
+var bcrypt = require('bcrypt')
+
+
+var userSchema = new schema({
+   name :{type :String ,require : true} ,
+   email :{type : String , require : true ,unique : true} ,
+   password : {type : String ,require : true} ,
+   age : Number ,
+   phone : Number
+
+} ,{timestamps : true})
+
+userSchema.pre('save' ,function(next) {
+    console.log(this ,'inside pre save')
+    if(this.password && this.isModified('password')){
+      bcrypt.hash(this.password ,10 ,(err ,hash) =>{
+          console.log(hash)
+          if(err) return next(err)
+          this.password = hash ;
+          return next()
+      })
+
+    } else {
+       next()
+    }
+})
+
+userSchema.methods.verifyPassword = function(password ,cb) {
+  bcrypt.compare(password ,this.password ,(err ,result) => {
+    return cb(err ,result)
+  })
+}
+
+var User = mongoose.model('User' ,userSchema)
+module.exports = User;
